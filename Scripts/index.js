f5380fb7f7132a75e905fb4f6500e98b8238db31
Waelen,Maurice M.A.E.M <m.waelen@student.fontys.nl>
2019-11-04 10:05:56 +0100

    added assets

---
+++ Scripts/index.js
@@ -0,0 +1,29 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+var tl = new TimelineMax({
+  repeat: -1
+});
+
+tl.add(
+  TweenMax.from(".logo-svg", 2, {
+    scale: 0.5,
+    rotation: 360,
+    ease: Elastic.easeInOut
+  })
+);
+
+tl.add(
+  TweenMax.to(".logo-svg", 2, {
+    scale: 0.5,
+    rotation: 360,
+    ease: Elastic.easeInOut
+  })
+);
