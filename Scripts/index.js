a649ebbf24732d942e814bde52da8a057a54b5ed
Waelen,Maurice M.A.E.M <m.waelen@student.fontys.nl>
2019-11-05 10:27:46 +0100

    SCO & UEX updated
    big picture

--- Scripts/index.js
+++ Scripts/index.js
@@ -7,23 +7,3 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
-
-var tl = new TimelineMax({
-  repeat: -1
-});
-
-tl.add(
-  TweenMax.from(".logo-svg", 2, {
-    scale: 0.5,
-    rotation: 360,
-    ease: Elastic.easeInOut
-  })
-);
-
-tl.add(
-  TweenMax.to(".logo-svg", 2, {
-    scale: 0.5,
-    rotation: 360,
-    ease: Elastic.easeInOut
-  })
-);
