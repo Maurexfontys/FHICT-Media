b4eee301986e1e9bb7bb373f521d3a27a4e2ca5f
Waelen,Maurice M.A.E.M <m.waelen@student.fontys.nl>
2019-11-29 09:09:24 +0100

    removed some unused code

--- Scripts/index.js
+++ Scripts/index.js
@@ -8,20 +8,6 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
-var i = 0;
-var txt =
-  "Hallo, mijn naam is Maurice Waelen (20), ik studeer ICT & Media Design aan de Fontys Hogeschool ICT te Eindhoven.                                                     Naast mijn studie ben ik ook veel bezig met StinStin Multimedia. Een eigen bedrijfje dat met een paar vrienden is opgezet. Stinstin Multimedia Design is gevestigd in Haelen en biedt verschillende producten aan op het gebied van grafische vormgeving, film en fotografie. Wij kunnen daarmee een complete multimedia oplossing voor bedrijven/ particulieren bieden. Ook hebben wij een professionele kortfilm genaamd 2Fast4U gemaakt. Waar ik ook een scene in meespeel.                                                                   Deze kortfilm is ook geselecteerd voor het amateur filmfestival in Cannes (Frankrijk)."; /* The text */
-var speed = 15; /* The speed/duration of the effect in milliseconds */
-
-window.onload = function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("txt").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-    console.log("ok");
-  }
-};
-
 (function() {
   "use strict";
 
